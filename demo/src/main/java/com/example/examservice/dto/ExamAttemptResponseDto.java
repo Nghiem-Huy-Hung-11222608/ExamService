c101b975c872798406f773e254b5bc60a68ff775
Nghiem-Huy-Hung-11222608 <128773660+Nghiem-Huy-Hung-11222608@users.noreply.github.com>
2025-07-07 16:51:56 +0700

    Add files via upload

---
+++ demo/src/main/java/com/example/examservice/dto/ExamAttemptResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.examservice.dto;
+
+import java.util.List;
+
+public class ExamAttemptResponseDto {
+    public Long attemptId;
+    public int score;
+    public List<Integer> userAnswers;
+    public List<Integer> correctAnswers;
+    public int triesLeft;
+}
